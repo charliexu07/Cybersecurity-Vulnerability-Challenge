5e79628b544a5efc96c57eb807093fcde746ef96
Jiachen Xu <jiachen.xu@vanderbilt.edu>
2020-10-10 19:21:00 -0500

    First Commit

---
+++ src/main/java/com/mycompany/myapp/repository/ApplicationRepository.java
@@ -0,0 +1,14 @@
+package com.mycompany.myapp.repository;
+
+import com.mycompany.myapp.domain.Application;
+
+import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Spring Data  repository for the Application entity.
+ */
+@SuppressWarnings("unused")
+@Repository
+public interface ApplicationRepository extends JpaRepository<Application, Long> {
+}
